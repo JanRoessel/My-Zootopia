2033fabd9a4da6fccbad5ea152d59b8850712d03
JanRoessel <roessel.jan@gmail.com>
2026-03-23 08:29:05 +0100

    added animals, added new hmtl-file and serialized data in one step (both steps in this commit)

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -14,12 +14,24 @@ for dic in animals_data:
     print(f"Location: {", ".join(dic["locations"])}")
     if dic["characteristics"].get("type") != None:
         print(f"Type: {dic["characteristics"].get("type")}")
-
-
     print()
 
+with open("animals_template.html", "r") as file:
+    html_content = file.read()
 
-#Name: American Foxhound
-#Diet: Omnivore
-#Location: North-America
-#Type: Hound
+animals_string = ""
+for animal in animals_data:
+    animals_string += "<li class='cards__item'>"
+    animals_string += f"<div class='card__title'>{animal['name']}</div>"
+    animals_string += "<div class='card__text'>"
+    animals_string += f"<p>Diet: {animal['characteristics']['diet']}</p>"
+    animals_string += f"<p>Location: {', '.join(animal['locations'])}</p>"
+    if animal['characteristics'].get('type'):
+        animals_string += f"<p>Type: {animal['characteristics']['type']}</p>"
+    animals_string += "</div>"
+    animals_string += "</li>"
+
+new_html = html_content.replace("__REPLACE_ANIMALS_INFO__", animals_string)
+
+with open("animals.html", "w") as file:
+    file.write(new_html)
